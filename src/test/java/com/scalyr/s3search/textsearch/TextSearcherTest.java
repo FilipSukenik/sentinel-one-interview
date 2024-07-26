4b7fe26184080ba6e52d35938fffbf5b1a88ca20
f.sukenik <f.sukenik@sportradar.com>
2024-07-26 12:53:31 +0200

    chore: copy and paste all test code from original structure 💥

---
+++ src/test/java/com/scalyr/s3search/textsearch/TextSearcherTest.java
@@ -0,0 +1,33 @@
+package com.scalyr.s3search.textsearch;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ * Tests for TextSearcher.
+ *
+ * @copyright 2024 SentinelOne, Inc.
+ */
+public class TextSearcherTest {
+  @Test public void test() {
+    TextSearcher searcher = new TextSearcher("foo", false);
+
+    assertEquals(0, countMatchesInBlob(searcher, "", 0, 0));
+    assertEquals(2, countMatchesInBlob(searcher, "foo bar baz foo", 0, 0));
+    assertEquals(2, countMatchesInBlob(searcher, "foo bar baz foo", 123, 234));
+    assertEquals(0, countMatchesInBlob(searcher, "abcdefghijfoxyz", 10, 20));
+    assertEquals(0, countMatchesInBlob(searcher, "ofo", 10, 20));
+    assertEquals(3, countMatchesInBlob(searcher, "abcfoofoofoodef", 3, 7));
+  }
+
+  private int countMatchesInBlob(TextSearcher searcher, String textToSearch, int paddingBefore,
+                                    int paddingAfter) {
+    byte[] buffer = textToSearch.getBytes();
+    byte[] paddedBuffer = new byte[buffer.length + paddingBefore + paddingAfter];
+
+    for (int i = 0; i < paddedBuffer.length; i++)
+      paddedBuffer[i] = buffer[(i - paddingBefore + buffer.length * 1000) % buffer.length];
+
+    return searcher.countMatchesInBlob(paddedBuffer, paddingBefore, paddingBefore + buffer.length);
+  }
+}
