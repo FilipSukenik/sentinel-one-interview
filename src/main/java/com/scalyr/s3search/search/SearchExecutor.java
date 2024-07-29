0a9ab66fba6a193063d235dd1965e618a22340da
f.sukenik <f.sukenik@sportradar.com>
2024-07-29 18:45:06 +0200

    refactor: move single task logic into own class

--- src/main/java/com/scalyr/s3search/search/SearchExecutor.java
+++ src/main/java/com/scalyr/s3search/search/SearchExecutor.java
@@ -5,9 +5,11 @@ import com.scalyr.s3search.textsearch.TextSearcher;
 import java.util.Arrays;
 
 public class SearchExecutor {
+
   private static final double NETWORK_FAILURE_RATE = 0.0025;
 
-  public void execute (String[] args) throws SimulatedS3Client.FlakyNetworkException {
+  public void execute(String[] args) throws SimulatedS3Client.FlakyNetworkException {
+
     String searchTerm = args.length > 0 ? args[0] : "pewter";
 
     SimulatedS3Client s3Client = new SimulatedS3Client(NETWORK_FAILURE_RATE);
@@ -20,20 +22,22 @@ public class SearchExecutor {
   /**
    * Return the number of matches for searchString in each of a series of "epochs" stored in Amazon S3.
    *
-   * @param startEpoch Index of the first epoch to search (inclusive).
-   * @param endEpoch Index just after the last epoch to search (exclusive).
+   * @param startEpoch   Index of the first epoch to search (inclusive).
+   * @param endEpoch     Index just after the last epoch to search (exclusive).
    * @param searchString String to search for.
-   * @param s3Client Accessor used to read the files to search.
+   * @param s3Client     Accessor used to read the files to search.
    * @return The count of matches across all epochs.
    */
   public int countMatchesInEpochs(int startEpoch, int endEpoch, String searchString,
-    SimulatedS3Client s3Client) throws SimulatedS3Client.FlakyNetworkException {
+    SimulatedS3Client s3Client) {
+
     TextSearcher searcher = new TextSearcher(searchString);
 
     int[] results = new int[endEpoch - startEpoch];
     for (int epochIndex = startEpoch; epochIndex < endEpoch; epochIndex++) {
-      byte[] epochData = s3Client.readFileFromS3("s3SimulationFiles", "epoch_" + epochIndex);
-      results[epochIndex - startEpoch] = searcher.countMatchesInBlob(epochData, 0, epochData.length);
+      var task = new SearchTask(s3Client, searcher, epochIndex);
+      task.run();
+      results[epochIndex - startEpoch] = task.getResult();
     }
 
     return Arrays.stream(results).sum();
